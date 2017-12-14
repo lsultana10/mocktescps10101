09de81c6f0131e36867debdc02b09ccd2a59a147
Larasultana <32870192+Larasultana@users.noreply.github.com>
2017-12-14 13:09:36 +0100

    no message

---
+++ mocktest/cmake-build-debug/CMakeFiles/mocktest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mocktest.dir/main.c.o"
+  "mocktest.pdb"
+  "mocktest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mocktest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
